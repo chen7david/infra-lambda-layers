7e32e1df5697aa2719102bc37e4b4b55cab382b4
David M. Chen <chen7david@me.com>
2023-10-12 12:30:22 +0300

    create: added layer construct to stack

--- lib/lambda-layer-example-stack.ts
+++ lib/lambda-layer-example-stack.ts
@@ -1,8 +1,25 @@
 import * as cdk from 'aws-cdk-lib'
 import { Construct } from 'constructs'
+import { aws_lambda as lambda } from 'aws-cdk-lib'
+import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs'
+import * as path from 'path'
 
 export class LambdaLayerExampleStack extends cdk.Stack {
+
+    public readonly layer: lambda.LayerVersion
+
     constructor(scope: Construct, id: string, props: cdk.StackProps){
         super(scope, id, props)
+
+        this.layer = new lambda.LayerVersion(this, 'helper-layer', {
+            layerVersionName: 'helper-alyer-two',
+            description: 'private helper function are help here',
+            code: lambda.Code.fromAsset('src/layers/helpers'),
+            compatibleRuntimes: [
+                lambda.Runtime.NODEJS_14_X,
+                lambda.Runtime.NODEJS_16_X,
+                lambda.Runtime.NODEJS_18_X,
+            ]
+        })
     }
 }
