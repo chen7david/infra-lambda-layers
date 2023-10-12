dc7a9ecf0c94704603b3231ebe0cc9a2702f5561
David M. Chen <chen7david@me.com>
2023-10-12 11:59:32 +0300

    chore: init project

---
+++ test/infra-lambda-layers.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as InfraLambdaLayers from '../lib/infra-lambda-layers-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/infra-lambda-layers-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new InfraLambdaLayers.InfraLambdaLayersStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
